45a0d91d66c12d99821556fd394f111b3923a4b8
Reinhold Lehnhardt-Niederstebruch <reinholdlehnhardt-niederstebruch@super.local>
2017-07-24 12:45:17 +0200

    laravel Init

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// nach / kommt die Seite like 'about'....
+// will den view welcome laden
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// auch möglich über einen PagesController..
+Route::get('/spieler', function () {
+    return view('spieler');
+});
+
+Route::get('/spielfeld', function () {
+    return view('spielfelder');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
