bc5a1bbba58216e2c29ea05542a3d998ddd32662
Zepryit <maximilian.kleindopp@wabsolute.de>
2017-07-25 15:06:41 +0200

    Spiler Ansicht und Spilerprofil-Ansicht erstellt

--- resources/views/players/index.blade.php
+++ resources/views/players/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layout')
 
-@section('palyer')
+@section('content')
+    <table class="table">
+        <thead>
+            <tr>
+                <th> Vorname </th>
+                <th> Nachname </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($players as $player)
+                <tr>
+                    <td> {{ $player->firstname }} </td>
+                    <td> {{ $player->lastname }} </td>
+                    <td>  <a href="/spieler/{{ $player->id  }}"> Profil anzeigen </a></td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
 
 @endsection
--- resources/views/players/show.blade.php
+++ resources/views/players/show.blade.php
@@ -1,5 +1,22 @@
 @extends('layout')
 
-@section('palyer')
-
+@section('content')
+    <table class="table">
+        @foreach($player as $player)
+        <thead>
+            <th>{{$player->firstname}}</th>
+            <th>{{$player->lastname}}</th>
+        </thead>
+        @endforeach
+        <tbody>
+        @foreach($game as $game)
+            <tr>
+                <td>  {{ $game->Date  }}  </td>
+                <td>  {{ $game->Time  }}  </td>
+                <td>  {{ $game->Firstname  }}  </td>
+                <td>  {{ $game->Secondname  }}  </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 @endsection
