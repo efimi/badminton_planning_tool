8329d740515c0c88952f09b5f20932dc3514e5fe
Zepryit <maximilian.kleindopp@wabsolute.de>
2017-07-26 11:03:52 +0200

    Design Änderungen

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="col-8 col-md-6" id="sidebar">
     <div class="list-group">
         {{--  nur bei Login anzeigen lassen --}}
-            <a class="nav-link" href="/spieler/erstellen">Spieler erstellen</a>
-            <a class="nav-link" href="/spielfeld/erstellen">Spielfeld erstellen</a>
-            <a class="nav-link" href="/spiel/erstellen">Spiel erstellen</a>
+            <a class="list-group-item" href="/spieler/erstellen">Spieler erstellen</a>
+            <a class="list-group-item" href="/spielfeld/erstellen">Spielfeld erstellen</a>
+            <a class="list-group-item" href="/spiel/erstellen">Spiel erstellen</a>
     </div>
-</div><!--/span-->
+</div>
