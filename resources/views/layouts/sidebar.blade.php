044d3aa75ebf4b7bea8d3159d5d601e9abbdabb8
Zepryit <maximilian.kleindopp@wabsolute.de>
2017-07-31 10:58:44 +0200

    Bootstrap anpassungen

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,22 +1,21 @@
-
-        <ul class="nav flex-colum">
-            {{--  nur bei Login anzeigen lassen --}}
-            <li class="nav-item">
-                <div class="nav-item list-group-item list-group-item-success">
-                    <a class="nav-link list-group-item col-md-12" href="/spieler/erstellen">Spieler erstellen</a>
-                    <a class="nav-link list-group-item col-md-12" href="/spieler">Spieler anzeigen</a>
-                </div>
+<ul class="nav flex-colum">
+    {{--  nur bei Login anzeigen lassen --}}
+    <li class="nav-item">
+        <div class="nav-item list-group-item list-group-item-success">
+            <a class="nav-link list-group-item" href="/spieler/erstellen">Spieler erstellen</a>
+            <a class="nav-link list-group-item" href="/spieler">Spieler anzeigen</a>
+        </div>
         {{-- </ul>
             <ul class="nav flex-colum"> --}}
-                <div class="list-group-item active">
-                    <a class="nav-link list-group-item col-md-12" href="/spiel/erstellen">Spiel erstellen</a>
-                    <a class="nav-link list-group-item col-md-12" href="/spiel/anzeigen">Spiele anzeigen</a>
-                </div>
-            {{-- </ul>
-          <ul class="nav flex-colum"> --}}
-                <div class="list-group-item list-group-item-warning nav-item ">
-                    <a class="list-group-item col-md-12" href="/spielfeld/erstellen">Spielfeld erstellen</a>
-                    <a class="list-group-item col-md-12" href="/spielfeld">Spielfelder anzeigen</a>
-                </div>
-              </li>
-            </ul>
+        <div class="list-group-item active">
+            <a class="nav-link list-group-item" href="/spiel/erstellen">Spiel erstellen</a>
+            <a class="nav-link list-group-item" href="/spiel/anzeigen">Spiele anzeigen</a>
+        </div>
+        {{-- </ul>
+      <ul class="nav flex-colum"> --}}
+        <div class="list-group-item list-group-item-warning nav-item ">
+            <a class="list-group-item" href="/spielfeld/erstellen">Spielfeld erstellen</a>
+            <a class="list-group-item" href="/spielfeld">Spielfelder anzeigen</a>
+        </div>
+    </li>
+</ul>
